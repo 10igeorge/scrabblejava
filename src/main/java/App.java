6b73d74a9aa1550465dedb2d8d1a4fa499bca7e6
Anna Becker and Isabelle George <10izzygeorge@gmail.com>
2016-02-09 10:01:19 -0800

    pass spec for letters with value of two

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,13 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) {}
 
+  public static Integer scrabbleScore(char letter) {
+    Integer totalScore = 0;
+    if (letter == 'D' || letter == 'G') {
+      totalScore += 2;
+    }
+    return totalScore;
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,4 +2,27 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppTest {
+
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForALetter_2() {
+    App scrabble = new App();
+    Integer score = 2;
+    assertEquals(score, scrabble.scrabbleScore('D'));
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+//Return score for letter
+//Split a word into CharArray
+//Add letter values to create a total score
